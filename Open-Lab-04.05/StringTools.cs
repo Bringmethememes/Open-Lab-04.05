37f0191dc07770f1f5759a92c2c52cc863d06eb7
PC <PC@ANDREJKOVPOITA>
2020-10-24 00:49:08 +0200

    Keby som len vedel ako tozrobic cez char array :{

--- Open-Lab-04.05/StringTools.cs
+++ Open-Lab-04.05/StringTools.cs
@@ -6,7 +6,15 @@ namespace Open_Lab_04._05
     {
         public string Repeat(string orig, int n)
         {
-            throw new NotImplementedException();
+            //char[] pleb = new char[orig.Length * n];
+            string wut = null;
+            for (int i = 0; i < orig.Length; i++)
+            {
+                for (int y = 0; y < n; y++)
+                    wut = wut + orig[i];
+            }
+            string blyat = string.Join("", wut);
+            return blyat;
         }
     }
 }
